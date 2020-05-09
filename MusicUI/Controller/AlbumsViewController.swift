6a500d0395c7fd402c4ab8ad8e3202f4ed73937d
quocnam90dev <quocnam90developer@gmail.com>
2020-05-10 00:57:33 +0700

    initializing

---
+++ MusicUI/Controller/AlbumsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PlaylistViewController.swift
+//  MusicUI
+//
+//  Created by beastlion on 5/9/20.
+//  Copyright © 2020 beast.ios.dev. All rights reserved.
+//
+
+import UIKit
+
+class AlbumsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Albums"
+        setupView()
+        view.backgroundColor = .white
+    }
+    
+    func setupView() {
+    
+    }
+}
---
+++ MusicUI/Controller/PlaylistsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PlaylistViewController.swift
+//  MusicUI
+//
+//  Created by beastlion on 5/9/20.
+//  Copyright © 2020 beast.ios.dev. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Playlists"
+        setupView()
+        view.backgroundColor = .white
+    }
+    
+    func setupView() {
+    
+    }
+}
